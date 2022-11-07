72737d9f398b9d1f68bb646c6c3ac1590dbdac31
Md. Imrul Hasan <mohammadhasan.imrul@gmail.com>
2022-11-08 01:16:39 +0600

    database and databse user created

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9znyou2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('fotoHunter').collection('services');
+    }
+    finally {
+
+    }
+}
+
 
 app.get('/', (req, res) => {
     res.send('fotoHunter server is running')
