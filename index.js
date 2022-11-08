48e5f1a72a51b8a0fe816c7076d2dfa33a677e19
Md. Imrul Hasan <mohammadhasan.imrul@gmail.com>
2022-11-08 21:50:03 +0600

    data stored in database and get from database and shown them on slient site

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -21,10 +21,24 @@ async function run() {
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
+            const services = await cursor.limit(3).toArray();
             res.send(services);
 
-        })
+        });
+
+        app.get('/allservices', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const allServices = await cursor.toArray();
+            res.send(allServices);
+        });
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        });
     }
     finally {
 
